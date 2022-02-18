a8bedf070e0fcef221a5cf8de4abe8a68108e6ed
Kellie Yau <97064025+kellieyau@users.noreply.github.com>
2022-02-18 13:28:37 +1100

    Header and Button added
    The react app does not display the components added

---
+++ task-manager/src/components/Button.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const Button = (colour, text, onClick) => {
+  return (
+    <button 
+     onClick={onClick}
+     style= {{backgroundColor: colour}} 
+     className='btn'>{text}
+    </button>)
+}
+
+Button.defaultProps = {
+    colour:'steelblue',
+    text: 'Add'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    colour: PropTypes.string,
+    onClick: PropTypes.func,
+}
+export default Button
---
+++ task-manager/src/components/Header.js
@@ -0,0 +1,28 @@
+import Button from './Button'
+import PropTypes from 'prop-types'
+
+const Header = ({title}) => {
+    const onClick= () => {
+        console.log('Click')
+    }
+  return (
+    <header className='header'>
+        <h1 style={headingStyle}>{title}</h1>
+        <Button colour='green' text='Hi' onClick={onClick}/>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    'title': 'Task Manager',
+}
+
+Header.propTypes = {
+    'title': PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'black',
+    backgroundColor: 'red'
+}
+export default Header
